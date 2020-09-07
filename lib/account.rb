8d6a6ffd6813ccd8f8d0707840227df4714e8332
Hilda Amponsah <hilda.amp@hotmail.com>
2020-09-07 14:59:49 +0100

    deposit of 1000

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
 class Account
-
-  def empty?
-    0
+  def initialize
+    @balance = 0
   end
   
+  def empty?
+    @balance
+  end
+
+  def deposit(balance)
+    @balance = 1000
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 require './lib/account'
 
 RSpec.describe '.Account' do
-
+  let(:account) { Account.new }
   describe 'deposit' do
     it 'is newly created' do
-      account = Account.new
-      expect(account.empty?).to eq (0)
+      expect(account.empty?).to eq(0)
     end
   end
+
+    it 'deposits any amount I deposit' do
+      expect(account.deposit(1000)).to eq(1000)
+    end
 end
